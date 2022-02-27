47d35ca848eeb1aeddc6e0f724cd5db376e5f5bf
Stephen Lennon <salennon@hotmail.co.uk>
2022-02-27 10:19:22 +0000

    Added functionality to print key presses as pressed

--- recorder.py
+++ recorder.py
@@ -6,8 +6,8 @@ class Recorder():
     def __init__(self):
         #Init keyboard and mouse listeners
         self.m_listener = mouse.Listener(on_click = self.on_click)
-        # self.kb_listener = keyboard.Listener(on_press = self.on_press, 
-        #                      on_release = self.on_release)
+        self.kb_listener = keyboard.Listener(on_press = self.on_press, 
+                             on_release = self.on_release)
 
         self.recorded_moves = []
 
@@ -16,16 +16,21 @@ class Recorder():
         press_type  = 'pressed' if pressed else 'released'
         print(f'{button} mouse {press_type} at ({x},{y})')
 
-    # def on_press(self, key):
-    #     '''Performed on key press during recording'''
-    #     print('Key ')
+    def on_press(self, key):
+        '''Performed on key press during recording'''
+        print(f'Key {key} pressed')
+    
+    def on_release(self, key):
+        '''Performed on key press during recording'''
+        print(f'Key {key} released')
 
         
 
 
     def record(self):
-        with self.m_listener as m_listener:
+        with self.m_listener as m_listener, self.kb_listener as kb_listener:
             m_listener.join()
+            kb_listener.join()
         #self.m_listener.start()
 
     #Load
